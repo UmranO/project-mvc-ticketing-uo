d11c942bea373a85b8eaab90fc164513ffd76920
Umran Ozman <umonal@yahoo.com>
2022-10-10 10:29:19 -0400

    CrudService Interface updated

--- src/main/java/com/cydeo/service/CrudService.java
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -1,4 +1,12 @@
 package com.cydeo.service;
 
-public interface CrudService {
+import java.util.List;
+
+public interface CrudService<T,ID> {
+
+    T save(T user);
+    T findById(ID username);
+    List<T> findAll();
+    void deleteById(ID username);
+
 }
