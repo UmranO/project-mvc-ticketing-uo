a923d7f7ff9ca19c93db6497730fffb1332292da
Umran Ozman <umonal@yahoo.com>
2022-10-10 08:44:05 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/ProjectMvcTicketingUoApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMvcTicketingUoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMvcTicketingUoApplication.class, args);
+    }
+
+}
