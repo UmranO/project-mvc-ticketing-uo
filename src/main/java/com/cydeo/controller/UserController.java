3505ad659ff3ecfe4d3105debfc420b8bea976c5
Umran Ozman <umonal@yahoo.com>
2022-10-13 19:40:59 -0400

    UserControl updated

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -48,8 +48,16 @@ public class UserController {
         model.addAttribute("users",userService.findAll());
 
         return "/user/update";
+
     }
 
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") UserDTO user){
+
+        userService.update(user);
+
+        return "redirect:/user/create";
+    }
 
 
 }
