3afb40da0d4f85742b037bc503b47997b67acf3a
Umran Ozman <umonal@yahoo.com>
2022-10-17 20:44:12 -0400

    TaskServiceImpl updated

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cydeo.service.impl;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -11,6 +14,12 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if(task.getTaskStatus() == null)
+            task.setTaskStatus(Status.OPEN);
+
+        if(task.getAssignedDate() == null)
+            task.setAssignedDate(LocalDate.now());
 
         return super.save(task.getId(),task);
 
